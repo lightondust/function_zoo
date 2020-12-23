460f8e7f44bb54bd6bbde2ca668a46559cbdb210
zs <lightondust@gmail.com>
2020-12-23 22:27:44 +0900

    add original video

--- functions/cubic.py
+++ functions/cubic.py
@@ -3,6 +3,7 @@ import plotly.express as px
 
 
 def cubic_function(st):
+    st.video('https://www.youtube.com/watch?v=vS0-M9SuV_s&t=4s')
     x = np.arange(-4.0, 4.0, 0.01)
 
     a = st.slider('a', -4.0, 4.0, step=0.01, value=1.0)
--- functions/e.py
+++ functions/e.py
@@ -3,6 +3,7 @@ import plotly.express as px
 
 
 def e(st):
+    st.video('https://www.youtube.com/watch?v=tjyYfU-MVa4')
     x = np.arange(1., 31., 1.)
     x = x ** 2
     max = st.slider('value to', 1, 30, 1)
